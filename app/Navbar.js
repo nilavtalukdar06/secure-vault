17c1f51c7962176c6157a34c5aae40db14a4aa87
Nilav <nilavtalukdar06@gmail.com>
2025-04-26 21:51:16 +0530

    called user creation api

--- app/Navbar.js
+++ app/Navbar.js
@@ -1,10 +1,42 @@
 "use client";
 import MobileMenu from "@/components/MobileMenu";
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, useUser } from "@clerk/nextjs";
+import { useEffect } from "react";
 import { Shield } from "lucide-react";
 import Link from "next/link";
+import toast from "react-hot-toast";
 
 export default function Navbar() {
+  const { user } = useUser();
+  const createUser = async () => {
+    try {
+      const response = await fetch("/api/create-user", {
+        method: "POST",
+        body: JSON.stringify({
+          name: user?.fullName || "user",
+          email: user?.primaryEmailAddress?.emailAddress,
+        }),
+        headers: {
+          "Content-type": "application/json",
+        },
+      });
+      if (!response.ok) {
+        throw new Error("Error creating user");
+      }
+      const data = await response.json();
+      if (data.status === 201) {
+        toast.success("User created successfully");
+      }
+    } catch (error) {
+      console.error(error);
+      toast.error("Failed to create user");
+    }
+  };
+
+  useEffect(() => {
+    createUser();
+  }, [user]);
+
   return (
     <header className="py-4 border-b border-purple-500 shadow-sm px-5 items-center bg-purple-500 text-white w-full flex justify-between">
       <Link className="sm:flex hidden sm:flex-1 justify-start" href="/">
--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Outfit } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
+import { Toaster } from "react-hot-toast";
 
 const outfit = Outfit({
   subsets: ["latin"],
@@ -18,6 +19,7 @@ export default function RootLayout({ children }) {
         <body className={`${outfit.className} antialiased`}>
           <main className="min-h-screen max-w-screen overflow-x-hidden relative">
             {children}
+            <Toaster />
           </main>
         </body>
       </html>
