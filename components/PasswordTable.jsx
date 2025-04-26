b06a54de5eb4c63c1b3821f42e9bf864dfca303a
Nilav <nilavtalukdar06@gmail.com>
2025-04-26 23:34:11 +0530

    Added password table

--- components/PasswordTable.jsx
+++ components/PasswordTable.jsx
@@ -4,92 +4,94 @@ import {
   TableBody,
   TableCaption,
   TableCell,
-  TableFooter,
   TableHead,
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
 import { Button } from "./ui/button";
-
-const invoices = [
-  {
-    invoice: "INV001",
-    paymentStatus: "Paid",
-    totalAmount: "$250.00",
-    paymentMethod: "Credit Card",
-  },
-  {
-    invoice: "INV002",
-    paymentStatus: "Pending",
-    totalAmount: "$150.00",
-    paymentMethod: "PayPal",
-  },
-  {
-    invoice: "INV003",
-    paymentStatus: "Unpaid",
-    totalAmount: "$350.00",
-    paymentMethod: "Bank Transfer",
-  },
-  {
-    invoice: "INV004",
-    paymentStatus: "Paid",
-    totalAmount: "$450.00",
-    paymentMethod: "Credit Card",
-  },
-  {
-    invoice: "INV005",
-    paymentStatus: "Paid",
-    totalAmount: "$550.00",
-    paymentMethod: "PayPal",
-  },
-  {
-    invoice: "INV006",
-    paymentStatus: "Pending",
-    totalAmount: "$200.00",
-    paymentMethod: "Bank Transfer",
-  },
-  {
-    invoice: "INV007",
-    paymentStatus: "Unpaid",
-    totalAmount: "$300.00",
-    paymentMethod: "Credit Card",
-  },
-];
+import { useEffect, useState } from "react";
+import { FadeLoader } from "react-spinners";
+import { useUser } from "@clerk/nextjs";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
 
 export function PasswordTable() {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [passwords, setPasswords] = useState([]);
+  const { user } = useUser();
+
+  const fetchPasswords = async (user) => {
+    try {
+      setLoading(true);
+      const response = await fetch(
+        `/api/get-passwords?email=${user?.primaryEmailAddress?.emailAddress}`
+      );
+      if (!response.ok) {
+        throw new Error("Passwords not found");
+      }
+      const data = await response.json();
+      if (!data.passwords.length) {
+        router.push("/");
+        toast.error("Create a password first");
+      }
+      setPasswords(data.passwords);
+    } catch (error) {
+      console.error(error);
+      toast.error("Error fetching passwords");
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    user && fetchPasswords(user);
+    console.log(passwords);
+  }, [user]);
+
   return (
-    <Table>
-      <TableCaption>A list of your recent invoices.</TableCaption>
-      <TableHeader>
-        <TableRow>
-          <TableHead className="w-[100px]">Invoice</TableHead>
-          <TableHead>Status</TableHead>
-          <TableHead>Method</TableHead>
-          <TableHead className="text-right">Amount</TableHead>
-          <TableHead className="text-right">Delete</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {invoices.map((invoice) => (
-          <TableRow key={invoice.invoice}>
-            <TableCell className="font-medium">{invoice.invoice}</TableCell>
-            <TableCell>{invoice.paymentStatus}</TableCell>
-            <TableCell>{invoice.paymentMethod}</TableCell>
-            <TableCell className="text-right">{invoice.totalAmount}</TableCell>
-            <TableCell className="text-right">
-              <Button variant="destructive" className="cursor-pointer">
-                Delete ☠️
-              </Button>
-            </TableCell>
-          </TableRow>
-        ))}
-      </TableBody>
-      <TableFooter>
-        <TableRow>
-          <TableCell colSpan={3}>Total</TableCell>
-          <TableCell className="text-right">$2,500.00</TableCell>
-        </TableRow>
-      </TableFooter>
-    </Table>
+    <div>
+      {loading ? (
+        <div className="w-full flex justify-center overflow-hidden h-full">
+          <FadeLoader />
+        </div>
+      ) : error ? (
+        <div className="my-10 text-red-500 font-bold text-2xl tracking-wider flex gap-x-2 justify-center items-center">
+          <span>Passwords not found 😓🥲</span>{" "}
+        </div>
+      ) : (
+        <Table>
+          <TableCaption>A list of your recent passwords</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px]">Serial</TableHead>
+              <TableHead>Website</TableHead>
+              <TableHead>Username</TableHead>
+              <TableHead className="text-right">Password</TableHead>
+              <TableHead className="text-right">Delete</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {passwords.map((password, index) => (
+              <TableRow key={password._id}>
+                <TableCell className="font-medium">{index + 1}</TableCell>
+                <TableCell>{password.website}</TableCell>
+                <TableCell>{password.username}</TableCell>
+                <TableCell className="text-right">
+                  {password.password}
+                </TableCell>
+                <TableCell className="text-right">
+                  <Button variant="destructive" className="cursor-pointer">
+                    Delete ☠️
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      )}
+    </div>
   );
 }
