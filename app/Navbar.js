16ceb141ea1ec941afe4d6f0cfbb13d7e8a08d0a
Nilav <nilavtalukdar06@gmail.com>
2025-04-26 22:14:56 +0530

    Added navbar

--- app/Navbar.js
+++ app/Navbar.js
@@ -1,41 +1,9 @@
-"use client";
 import MobileMenu from "@/components/MobileMenu";
-import { UserButton, useUser } from "@clerk/nextjs";
-import { useEffect } from "react";
+import { UserButton } from "@clerk/nextjs";
 import { Shield } from "lucide-react";
 import Link from "next/link";
-import toast from "react-hot-toast";
 
 export default function Navbar() {
-  const { user } = useUser();
-  const createUser = async () => {
-    try {
-      const response = await fetch("/api/create-user", {
-        method: "POST",
-        body: JSON.stringify({
-          name: user?.fullName || "user",
-          email: user?.primaryEmailAddress?.emailAddress,
-        }),
-        headers: {
-          "Content-type": "application/json",
-        },
-      });
-      if (!response.ok) {
-        throw new Error("Error creating user");
-      }
-      if (response.status === 201) {
-        toast.success("User created successfully");
-      }
-    } catch (error) {
-      console.error(error);
-      toast.error("Failed to create user");
-    }
-  };
-
-  useEffect(() => {
-    user && createUser();
-  }, [user]);
-
   return (
     <header className="py-4 border-b border-purple-500 shadow-sm px-5 items-center bg-purple-500 text-white w-full flex justify-between">
       <Link className="sm:flex hidden sm:flex-1 justify-start" href="/">
