dd9ee0476f6516be24e6e36d9175b2a3b77f2e7b
Nilav <nilavtalukdar06@gmail.com>
2025-07-13 19:59:39 +0530

    add shadcn components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <section>
       <h1>Secure Vault</h1>
+      <Button>Click Me</Button>
     </section>
   );
 }
