81e5eac7da28e19645077c06f6d8d7a7d7c858b5
Nilav <nilavtalukdar06@gmail.com>
2025-07-13 20:19:28 +0530

    added clerk user button

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <section>
       <h1>Secure Vault</h1>
       <Button>Click Me</Button>
+      <UserButton />
     </section>
   );
 }
