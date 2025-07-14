9c88633ebb08bbb5c1d261115829a23f60495ff9
Nilav <nilavtalukdar06@gmail.com>
2025-07-14 13:53:34 +0530

    add form schema

--- components/password/create-password.tsx
+++ components/password/create-password.tsx
@@ -1,6 +1,28 @@
 "use client";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+const formSchema = z.object({
+  name: z.string().min(1, { message: "Name is too short" }),
+  email: z.string().email({ message: "Email is not valid" }),
+  password: z.string().min(1, { message: "Password is too short" }),
+});
 
 export default function CreatePassword() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  }
+
   return (
     <div className="my-12">
       <div>Password</div>
