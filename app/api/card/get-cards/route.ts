ab8d037736fb2dd924aaa6057a07c519fe66c688
Nilav <nilavtalukdar06@gmail.com>
2025-07-14 21:41:25 +0530

    added api routes for getting cards

---
+++ app/api/card/get-cards/route.ts
@@ -0,0 +1,25 @@
+import connectToMongodb from "@/db/mongodb";
+import Card from "@/models/card.model";
+import { auth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const { userId } = await auth();
+    if (!userId) {
+      return NextResponse.json(
+        { error: "user is not authenticated" },
+        { status: 401 }
+      );
+    }
+    await connectToMongodb();
+    const result = await Card.find({});
+    return NextResponse.json(result, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "internal server error" },
+      { status: 500 }
+    );
+  }
+}
