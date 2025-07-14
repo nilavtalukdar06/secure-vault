d75f1000f0f193033d6325383788378f68e437f2
Nilav <nilavtalukdar06@gmail.com>
2025-07-14 15:25:45 +0530

    added form schema

--- components/card/create-card.tsx
+++ components/card/create-card.tsx
@@ -1,4 +1,41 @@
+"use client";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+const formSchema = z.object({
+  name: z.string().min(1, { message: "Name is too short" }),
+  cardNumber: z
+    .string()
+    .min(1, { message: "Card number is too short" })
+    .regex(/^(?:\d[ -]*?){13,19}$/, { message: "Enter a valid card number" }),
+  cvv: z
+    .string()
+    .min(1, { message: "CVV is too short" })
+    .regex(/^\d{3,4}$/, { message: "Invalid CVV type" }),
+  expiryDate: z
+    .string()
+    .min(1, { message: "Expiry date is too short" })
+    .regex(/^(0[1-9]|1[0-2])\/\d{2}$/, {
+      message: "Enter a valid expiry date",
+    }),
+});
+
 export default function CreateCard() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      cardNumber: "",
+      cvv: "",
+      expiryDate: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
   return (
     <div className="my-12">
       <p>Create Card</p>
