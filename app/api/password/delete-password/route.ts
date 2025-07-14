9e79a143276a6d5e07e0e67ad1fc6f3c4ac1e5e0
Nilav <nilavtalukdar06@gmail.com>
2025-07-14 22:24:18 +0530

    added DELETE API routes for card and password models

---
+++ app/api/password/delete-password/route.ts
@@ -0,0 +1,35 @@
+import connectToMongodb from "@/db/mongodb";
+import Password from "@/models/password.model";
+import { auth } from "@clerk/nextjs/server";
+import { NextResponse, NextRequest } from "next/server";
+
+export async function DELETE(request: NextRequest) {
+  try {
+    const { userId } = await auth();
+    if (!userId) {
+      return NextResponse.json(
+        { error: "user is not authenticated" },
+        { status: 401 }
+      );
+    }
+    const documentId = request.nextUrl.searchParams.get("documentId");
+    if (!documentId) {
+      return NextResponse.json(
+        { error: "document id is not present" },
+        { status: 400 }
+      );
+    }
+    await connectToMongodb();
+    await Password.findByIdAndDelete(documentId);
+    return NextResponse.json(
+      { message: "password details deleted successfully" },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "internal server error" },
+      { status: 500 }
+    );
+  }
+}
