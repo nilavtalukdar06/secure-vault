2feda119eea95e13d669e17a3fad24b50f943b48
Nilav <nilavtalukdar06@gmail.com>
2025-07-14 14:12:19 +0530

    added form ui

--- components/password/create-password.tsx
+++ components/password/create-password.tsx
@@ -2,6 +2,17 @@
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { ShieldCheck } from "lucide-react";
 
 const formSchema = z.object({
   name: z.string().min(1, { message: "Name is too short" }),
@@ -21,11 +32,68 @@ export default function CreatePassword() {
 
   const onSubmit = (values: z.infer<typeof formSchema>) => {
     console.log(values);
-  }
+  };
 
   return (
     <div className="my-12">
-      <div>Password</div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-5">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Enter name</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="Ex: Instagram, Github"
+                    {...field}
+                    type="text"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Enter email</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="Ex: johndoe123@example.com"
+                    {...field}
+                    type="email"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Enter password</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder="Enter a strong password"
+                    {...field}
+                    type="password"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button variant="outline" className={"w-full"}>
+            Add Password <ShieldCheck />
+          </Button>
+        </form>
+      </Form>
     </div>
   );
 }
