31bff563206be56dd56eba592eb60733bd9a7fab
Nilav <nilavtalukdar06@gmail.com>
2025-07-14 13:47:56 +0530

    added password creation component

--- app/(routes)/add-password/page.tsx
+++ app/(routes)/add-password/page.tsx
@@ -1,7 +1,8 @@
 import { Button } from "@/components/ui/button";
 import { ShieldCheck } from "lucide-react";
+import CreatePassword from "@/components/password/create-password";
 
-export default function CreatePassword() {
+export default function AddPassword() {
   return (
     <section className="w-full">
       <div className="my-16 max-w-sm mx-auto w-full">
@@ -11,6 +12,7 @@ export default function CreatePassword() {
           </Button>
           <h2 className="text-xl font-normal text-gray-600">Add Password</h2>
         </div>
+        <CreatePassword />
       </div>
     </section>
   );
---
+++ components/password/create-password.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function CreatePassword() {
+  return (
+    <div className="my-12">
+      <div>Password</div>
+    </div>
+  );
+}
