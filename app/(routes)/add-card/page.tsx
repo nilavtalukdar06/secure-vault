7bbd51dd920a7fbdae0b91a656d74ca889fd89e7
Nilav <nilavtalukdar06@gmail.com>
2025-07-14 15:07:01 +0530

    added add card route

---
+++ app/(routes)/add-card/page.tsx
@@ -0,0 +1,17 @@
+import { Button } from "@/components/ui/button";
+import { CreditCard } from "lucide-react";
+
+export default function AddPassword() {
+  return (
+    <section className="w-full">
+      <div className="my-16 max-w-sm mx-auto w-full">
+        <div className="flex justify-center items-center gap-x-4">
+          <Button variant="outline" className="pointer-events-none" size="sm">
+            <CreditCard />
+          </Button>
+          <h2 className="text-xl font-normal text-gray-600">Add Card</h2>
+        </div>
+      </div>
+    </section>
+  );
+}
