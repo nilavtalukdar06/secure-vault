402157b0123763b6fdcd9a8b8cdce5cb369e8d5e
Nilav <nilavtalukdar06@gmail.com>
2025-07-14 21:37:34 +0530

    added api routes for getting passwords

---
+++ app/api/password/get-passwords/route.ts
@@ -0,0 +1,25 @@
+import connectToMongodb from "@/db/mongodb";
+import Password from "@/models/password.model";
+import { auth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const { userId } = await auth();
+    if (!userId) {
+      return NextResponse.json(
+        { error: "user is not authenticated" },
+        { status: 401 }
+      );
+    }
+    await connectToMongodb();
+    const result = await Password.find({});
+    return NextResponse.json(result, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "internal server error" },
+      { status: 500 }
+    );
+  }
+}
