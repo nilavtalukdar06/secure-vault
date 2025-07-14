a1601e65fb417ecb1bfd2a1547b611b05b534fa8
Nilav <nilavtalukdar06@gmail.com>
2025-07-14 11:51:26 +0530

    updated clerk sign in page

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -2,7 +2,7 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function SignInPage() {
   return (
-    <section className="min-h-screen max-w-screen overflow-x-hidden relative flex justify-center items-center">
+    <section className="min-h-screen max-w-screen flex justify-center items-center">
       <SignIn />
     </section>
   );
